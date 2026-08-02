025b818296c308a94642d4d81771daa8b15694a9
Panos Economou <panos.economou.v@gmail.com>
2026-08-01 23:45:34 -0400

    wayland keymap support and decoder prettification

--- include/decoder.h
+++ include/decoder.h
@@ -1,24 +1,66 @@
+/* ╔══════════════════════════════════════════════════════════════════════════════╗ *
+ * ║                                                                              ║ *
+ * ║   ┏━┛┏━ ┛┏━┃┏━┃┃ ┃                                                           ║ *
+ * ║   ━━┃┃ ┃┃┏━┛┏━┛━┏┛                                                           ║ *
+ * ║   ━━┛┛ ┛┛┛  ┛   ┛                                                            ║ *
+ * ║   decoder.h | talk to wayland to understand wtf they keys are saying         ║ *
+ * ║                                                                              ║ *
+ * ╚══════════════════════════════════════════════════════════════════════════════╝ */
+
 #ifndef SNIPPY_DECODER_H
 #define SNIPPY_DECODER_H
 
-#include "keyboard.h"
+/* -----  Headers  ──────────────────────────────────────────────────────────────── */
 
-#define XKB_KEYCODE_OFFSET 8
+#include "keyboard.h"                       /* struct keyboard                      */
+
+/* ─────  Constants  ────────────────────────────────────────────────────────────── */
+
+#define XKB_KEYCODE_OFFSET  8               /* diff between evdev and xkb keycode   */
+
+
+/* ─────  Structures   ──────────────────────────────────────────────────────────── */
 
 struct decoder {
-    struct xkb_context*     context;
-    struct xkb_keymap*      keymap;
-    struct wl_display*      display;
-    struct wl_seat*         seat;
-    struct wl_keyboard*     keyboard;
+    struct xkb_context*     context;        /* NULL when starting or unset          */
+    struct xkb_keymap*      keymap;         /* current keymap                       */
+    struct wl_display*      display;        /* wayland access point                 */
+    struct wl_registry*     registry;       /* wayland access point                 */
+    struct wl_seat*         seat;           /* even further wayland access point    */
+    struct wl_keyboard*     keyboard;       /* correponding device                  */
 };
 
-int                 decoder_init        (struct decoder*           decoder);
-void                decoder_cleanup     (struct decoder*           decoder);
-struct xkb_keymap*  decoder_get_keymap  (struct xkb_context*       context);
-void                decoder_keymap_unref(struct xkb_keymap*        keymap);
-int                 decoder_event       (struct keyboard*          keyboard, 
-                                         const struct input_event* event,
-                                         char *buffer, size_t buffer_size);
+/* ─────  Decoders  ─────────────────────────────────────────────────────────────── */
+
+int  decoder_init    (struct decoder* decoder);
+void decoder_cleanup (struct decoder* decoder);
+
+/* ─────  Decoder Functionality  ────────────────────────────────────────────────── */
+
+/*
+ * Gets a keymap directly from xkb defaults as a fallback when wayland is being
+ * weird.
+ *
+ * @return xkb_keymap with hopefully no leaks.
+ */
+
+struct xkb_keymap*  decoder_get_keymap   (struct xkb_context*       context);
+
+
+/*
+ * Given a valid keyboard event obtain what character was actually pressed.
+ * Honestly if I was writing this in python, this would probably be the only 
+ * method in the code lmao
+ *
+ * @return the number of characters the corresponding keycode needs (e.g. 0 for 
+ * SHIFT, 1 for a, etc.)
+ */
+
+int decoder_event (struct keyboard*          keyboard, 
+                   const struct input_event* event,
+                   char*                     buffer, 
+                   size_t                    buffer_size);
 
 #endif
+
+/* ──────────────────────────────────────────────────────────────────────────────── */
